43deede579cd9a3fa1b547919c56419d7d26efcf
MountainGator <luketaylorsanders@gmail.com>
2022-04-12 20:55:56 -0500

    solve exercises

---
+++ greet/app.py
@@ -0,0 +1,36 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    html = """
+    <!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta http-equiv="X-UA-Compatible" content="IE=edge">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>Document</title>
+        </head>
+        <body>
+            <h1>Flask Practice!</h1>
+            <a href="/welcome">suprise page</a>
+            <a href="/welcome/home">another suprise</a>
+            <a href="/welcome/back">third suprise</a>
+        </body>
+        </html>
+    """
+    return html
+
+@app.route('/<welcome>')
+def welcome(welcome):
+    return f"<h2>you reached the {welcome} page"
+
+@app.route('/welcome/<home>')
+def welcome_home(home):
+    return f"welcome {home} you filthy animal"
+
+@app.route('/welcome/<back>')
+def welcome_back(back):
+    return f"welcome {back}, you sly dog"
